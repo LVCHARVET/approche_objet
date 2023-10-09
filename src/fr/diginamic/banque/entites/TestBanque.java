b3e4dc6b1a35c968d78e97daf82597229cf27208
Lv CHARVET <lv.charvet28@gmail.com>
2023-10-09 13:47:52 +0200

    TP7 modif

--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -4,14 +4,13 @@ public class TestBanque {
 
 	public static void main(String[] args) {
 		Compte[] arrayCompte;
-		int i;
 
 		arrayCompte = new Compte[2];
 		arrayCompte[0] = new Compte(123456, 2000.20);
 		arrayCompte[1] = new CompteTaux(223456, 2222.22, 2);
 
-		for (i = 0; i < arrayCompte.length; i++) {
-			System.out.println(arrayCompte[i]);
+		for (Compte comptes : arrayCompte) {
+			System.out.println(comptes);
 		}
 
 	}
