75e4fd481f75c4e7d209bb585a44ed497c69378c
Lv CHARVET <lv.charvet28@gmail.com>
2023-10-17 11:29:08 +0200

    TP StringBuilder

---
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -0,0 +1,31 @@
+package fr.diginamic.chaines;
+
+public class TestStringBuilder {
+
+	public static void main(String[] args) {
+		long startTimeString = System.currentTimeMillis();
+		String nbString = null;
+		for (int i = 1; i <= 100000; i++) {
+			nbString = nbString + i;
+		}
+		long endTimeString = System.currentTimeMillis();
+		long executionTimeString = endTimeString - startTimeString;
+
+		System.out.println("Temps écoulé avec concat : " + executionTimeString + " millisecondes");
+
+		System.out.println("---------------");
+
+		long startTimeAppend = System.currentTimeMillis();
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (int i = 1; i <= 100000; i++) {
+			stringBuilder.append(i);
+		}
+
+		long endTimeAppend = System.currentTimeMillis();
+		long executionTimeAppend = endTimeAppend - startTimeAppend;
+
+		System.out.println("Temps écoulé avec append : " + executionTimeAppend + " millisecondes");
+	}
+
+}
